c8db94c0f8c3c174c9a3fe89462c71f44f4d835d
usha-12 <usha12ahirwar@gmail.com>
2022-11-03 20:33:15 +0530

    Let's create a simple linkedlist list of 56, 30 and 70

---
+++ src/com/bredeglabz/linkedlist/LinkedList.java
@@ -0,0 +1,32 @@
+package com.bredeglabz.linkedlist;
+
+public class LinkedList <T>{
+    Node<T> head;
+    Node<T> tail;
+    public void push(T data) {
+        Node<T> newMyNode = new Node(data);
+        if (head == null) {
+            head = newMyNode;
+            tail = newMyNode;
+        } else {
+            newMyNode.setNext(head);
+            head = newMyNode;
+        }
+    }
+    public void print(){
+        if (head == null){
+            System.out.println("Linked List is Empty");
+        }else {
+            Node<T> currNode = head;
+            while (currNode != null){
+                System.out.print(currNode.getData()+" ");
+                currNode = currNode.getNext();
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+}
--- src/com/bredeglabz/linkedlist/LinkedListMain.java
+++ src/com/bredeglabz/linkedlist/LinkedListMain.java
@@ -1,2 +1,12 @@
-package com.bredeglabz.linkedlist;public class LinkedListMain {
+package com.bredeglabz.linkedlist;
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("---------Welcome to the LinkedList program---------");
+        LinkedList<Integer> list1 = new LinkedList();
+        System.out.println("--------------Create Linked List----------------");
+        list1.push(56);
+        list1.push(30);
+        list1.push(70);
+        list1.print();
+    }
 }
