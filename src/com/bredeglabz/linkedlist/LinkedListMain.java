45bc533617447c752bf4e44e8637ea8362a5e28e
usha-12 <usha12ahirwar@gmail.com>
2022-11-03 23:05:54 +0530

    inset 40 after 30 to the linked list

--- src/com/bredeglabz/linkedlist/LinkedListMain.java
+++ src/com/bredeglabz/linkedlist/LinkedListMain.java
@@ -8,17 +8,18 @@ public class LinkedListMain {
         list1.add(30);
         list1.add(70);
         list1.print();
-        System.out.println("------Searching for element 30 in the Linked List------");
-        list1.search(30);
+        System.out.println("------Inserting 40 after 30 in the Linked List------");
+        list1.search(40,30);
         list1.print();
 
     }
 }
-/*Ability to search
-LinkedList to find Node
-with value 30
-
-- Write Junit Test Case as
-demonstrated in class
-- Loop through LinkedList to
-find node with key 30*/
+/*Ability to insert 40 after 30 to
+the Linked List sequence of
+56->30->70
+- Write Junit Test Case as demonstrated
+in class
+- Search LinkedList to get Node with key
+value 30
+- Then Insert 40 to 30
+- Final Sequence: 56->30->40->70*/
